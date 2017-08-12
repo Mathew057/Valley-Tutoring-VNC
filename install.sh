f468f3dc885cb5171d23b1e92fb88e8e77122550
Mathew057 <mathew0057@gmail.com>
2017-08-12 02:17:06 -0500

    initial

---
+++ install.sh
@@ -0,0 +1,65 @@
+# @Author: Mathew Black
+# @Date:   2017-08-12T01:54:07-05:00
+# @Email:  dev@mathewblack.com
+# @Last modified by:   Mathew Black
+# @Last modified time: 2017-08-12T02:07:29-05:00
+# @License: MIT
+
+
+
+#!/bin/bash
+
+#Request sudo permissions
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
+#Get prerequisites
+
+#force update
+
+sudo apt-get update
+sudo apt-get install supervisor xvfb fluxbox x11vnc websockify
+
+#Copy supervisord configuration to proper configuration directory
+cp supervisord.conf ${HOME}/.config/supervisord.conf
+
+#Create the proper directory for the script
+sudo mkdir /opt/c9vnc
+
+#Make sure that the runners folder exists
+mkdir ${HOME}/workspace/.c9/runners
+
+#Copy the C9 runner to the C9 watch folder
+cp c9vnc.run ${HOME}/workspace/.c9/runners/c9vnc.run
+cp sdlremote.run ${HOME}/workspace/.c9/runners/sdlremote.run
+
+#Copy the fluxbox config
+cp fluxbox.conf ${HOME}/.fluxbox/startup
+
+#Copy the run script to proper /opt/ directory
+sudo cp run.sh /opt/c9vnc/c9vnc.sh
+
+#Copy the run script to proper /opt/ directory
+sudo cp uninstall.sh /opt/c9vnc/uninstall.sh
+
+#Support password for x11vnc
+sudo cp x11vncrun.sh /opt/c9vnc/x11vncrun.sh
+
+#Clone noVNC into proper /opt/ directory
+git clone git://github.com/kanaka/noVNC /opt/noVNC/
+
+#Set up password for x11vnc
+echo
+echo
+while true; do
+    read -p "Do you wish to set password for x11vnc? (not recommended for public workspaces!) " yn
+    case $yn in
+#        [Yy]* ) x11vnc -storepasswd; break;; Coming later
+        [Yy]* ) (echo Coming later); break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
---
+++ run.sh
@@ -0,0 +1,108 @@
+# @Author: Mathew Black
+# @Date:   2017-08-12T01:54:07-05:00
+# @Email:  dev@mathewblack.com
+# @Last modified by:   Mathew Black
+# @Last modified time: 2017-08-12T01:59:38-05:00
+# @License: MIT
+
+
+
+#!/bin/bash
+
+foreground=false
+
+function daemonStop {
+    {
+    #Stop all C9vnc sub-processes
+    supervisorctl stop novnc
+    supervisorctl stop fluxbox
+    supervisorctl stop x11vnc
+    supervisorctl stop xvfb
+
+    #If that fails, killall
+    killall Xvfb x11vnc websockify supervisord
+    }&> /dev/null
+}
+
+function finish {
+    #Revert back to original
+    mv ${HOME}/.fluxbox/startup.bak ${HOME}/.fluxbox/startup
+    if ! $foreground; then
+        daemonStop
+    fi
+    echo "finished cleanly"
+}
+trap finish SIGINT SIGTERM
+
+command=$1
+
+function showHelp {
+    echo -e "Usage: c9vnc <args>"
+    echo -e "   -h          Print this message"
+    echo -e "   -f          Run in the foreground"
+    echo -e "   -k          Kill running daemon"
+    echo -e "No arguments will try to start daemon process"
+}
+
+function runningMessage {
+    echo VNC client running at https://$C9_HOSTNAME/vnc.html?autoconnect=true
+}
+
+function foregroundStart {
+
+    #Run C9vnc in foreground
+
+    #Set environment variables
+    export XDG_RUNTIME_DIR=/tmp/C9VNC
+    export DISPLAY=:99.0
+
+    #Back up existing configs
+    cp ${HOME}/.fluxbox/startup ${HOME}/.fluxbox/startup.bak
+    cp ${HOME}/.config/supervisord.conf ${HOME}/.config/supervisord.conf.bak
+
+    echo "Running command $command on VNC"
+    #Modify fluxbox config
+    sed -i -e "s|somecommand|$command|g" ${HOME}/.fluxbox/startup
+
+    #Modify supervisord.conf
+    sed -i -e 's/nodaemon=false/nodaemon=true/' ${HOME}/.config/supervisord.conf
+
+    #Run supervisord
+    supervisord -c ${HOME}/.config/supervisord.conf
+}
+
+function daemonStart {
+    echo -e "Starting c9vnc daemon"
+    echo "Running command $command on VNC"
+    {
+    #Set environment variables
+    export XDG_RUNTIME_DIR=/tmp/C9VNC
+    export DISPLAY=:99.0
+
+    #Back up existing configs
+    cp ${HOME}/.fluxbox/startup ${HOME}/.fluxbox/startup.bak
+
+    #Modify fluxbox config
+    sed -i -e "s|somecommand|$command|g" ${HOME}/.fluxbox/startup
+
+    #Run supervisord
+    supervisord -c ${HOME}/.config/supervisord.conf
+    }&> /dev/null
+}
+
+
+
+while getopts :hfk opt; do
+    foreground=true
+    case $opt in
+        h) showHelp; exit ;;
+        f) runningMessage; foregroundStart ; exit;;
+        k) daemonStop ; exit ;;
+       \?) echo "Unknown option -$OPTARG"; exit 1;;
+    esac
+done
+
+# No arguments, default to starting in background
+if ! $foreground; then
+    runningMessage; daemonStart ; sleep 5 ;  exit ;
+fi
